f56f69788e829f83f5a4e5ad31a7e03e38c33469
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-12-13 16:12:01 +0300

    user structure

--- MaillApp/Program.cs
+++ MaillApp/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace MaillApp
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MaillApp
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
         }
+    }
+}
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public List<Message> Inbox { get; set; } = new List<Message>();
+    public List<Message> SentMessages { get; set; } = new List<Message>();
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
     }
 }
